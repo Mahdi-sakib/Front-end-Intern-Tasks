bba401e062a05222d5f99015777fe70362438349
Mahdi Sakib Rahman <75987463+Mahdi-sakib@users.noreply.github.com>
2022-12-04 19:03:37 +0600

    Add files via upload

---
+++ Form 2/SignupFormRootRoot1.js
@@ -0,0 +1,242 @@
+import React from "react";
+import styled from "styled-components";
+
+export const SignupFormRootRoot1 = ({}) => {
+  return (
+    <SignupFormRootRootRoot>
+      <FlexColumn>
+        <Istockphotox
+          src={`https://file.rendit.io/n/l1ZqOaFvGNnmMu1QOhj0.png`}
+        />
+        <UltimateHrmLogo
+          src={`https://file.rendit.io/n/OI5Nl8NLdiApeekcpy8G.png`}
+        />
+      </FlexColumn>
+      <WhiteFlexColumn>
+        <Text1>SignUp Form</Text1>
+        <FlexRow>
+          <FlexColumn1>
+            <Text2>+880</Text2>
+            <Image1 src={`https://file.rendit.io/n/GEqcGwPu3gavIpBph8mQ.svg`} />
+          </FlexColumn1>
+          <FlexColumn2>
+            <FlexColumn3>
+              <Text3>1xxxxxxxxxx</Text3>
+              <Image2
+                src={`https://file.rendit.io/n/hFzzGhfVcaEptJmibTHR.svg`}
+              />
+            </FlexColumn3>
+            <Image3 src={`https://file.rendit.io/n/vkXFeNZBccrUwvIuylCf.svg`} />
+          </FlexColumn2>
+        </FlexRow>
+        <FlexColumn4>
+          <FlexColumn5>
+            <Text4>Write Email Address</Text4>
+            <Image2 src={`https://file.rendit.io/n/hFzzGhfVcaEptJmibTHR.svg`} />
+          </FlexColumn5>
+          <Image5 src={`https://file.rendit.io/n/cU2uTFy3xMRjUj8nMXG2.svg`} />
+        </FlexColumn4>
+        <FlexRow1>
+          <Text5>Back</Text5>
+          <NavyBlueFlexRow>
+            <Text6>Next Step</Text6>
+            <Arrow src={`https://file.rendit.io/n/Xc1qo7sXo2krjMIQr9WG.svg`} />
+          </NavyBlueFlexRow>
+        </FlexRow1>
+      </WhiteFlexColumn>
+    </SignupFormRootRootRoot>
+  );
+};
+
+const Image2 = styled.img`
+  width: 1px;
+  height: 14px;
+  position: relative;
+`;
+const SignupFormRootRootRoot = styled.div`
+  width: 1349px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 19px 35px 11px 56px;
+  background-color: #ffffff;
+  overflow: hidden;
+`;
+const FlexColumn = styled.div`
+  width: 164px;
+  height: 544px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 43px 520px 43px 0px;
+`;
+const Istockphotox = styled.img`
+  width: 612px;
+  height: 437px;
+  left: 72px;
+  top: 91px;
+  position: absolute;
+`;
+const UltimateHrmLogo = styled.img`
+  width: 164px;
+  height: 60px;
+  position: relative;
+`;
+const WhiteFlexColumn = styled.div`
+  width: 369px;
+  height: 412px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 109px 73.5px 109px 74px;
+  background-color: #ffffff;
+  box-shadow: -1px 10px 25px 0px rgba(147, 147, 147, 0.25),
+    1px -10px 25px 0px rgba(201, 201, 201, 0.25);
+`;
+const Text1 = styled.div`
+  margin: 0px 0px 99px 122px;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: Inter;
+  white-space: nowrap;
+`;
+const FlexRow = styled.div`
+  gap: 3px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 0px 64px 0px;
+`;
+const FlexColumn1 = styled.div`
+  width: 90.5px;
+  height: 21px;
+  gap: 1.5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Text2 = styled.div`
+  align-self: flex-start;
+  margin: 0px 0px 0px 15px;
+  color: #b3b3b3;
+  font-size: 16px;
+  font-family: Inter;
+  white-space: nowrap;
+`;
+const Image1 = styled.img`
+  width: 91px;
+  height: 1px;
+  margin: 0px 0px 0px -0.5px;
+`;
+const FlexColumn2 = styled.div`
+  width: 272px;
+  height: 21px;
+  gap: 1.5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const FlexColumn3 = styled.div`
+  width: 1px;
+  height: 19px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: stretch;
+  padding: 0px 258px 0px 13px;
+`;
+const Text3 = styled.div`
+  left: 13.5px;
+  top: 0px;
+  position: absolute;
+  color: #b3b3b3;
+  font-size: 16px;
+  font-family: Inter;
+  white-space: nowrap;
+`;
+const Image3 = styled.img`
+  width: 272px;
+  height: 1px;
+  margin: 0px -0.5px 0px 0px;
+`;
+const FlexColumn4 = styled.div`
+  width: 369px;
+  height: 20px;
+  gap: 0.5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: stretch;
+  align-items: center;
+  margin: 0px 0px 68px 0px;
+`;
+const FlexColumn5 = styled.div`
+  width: 1px;
+  height: 16px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-self: stretch;
+  padding: 1.5px 353px 1.5px 14.5px;
+`;
+const Text4 = styled.div`
+  left: 15px;
+  top: 0px;
+  position: absolute;
+  color: #b3b3b3;
+  font-size: 16px;
+  font-family: Inter;
+  white-space: nowrap;
+`;
+const Image5 = styled.img`
+  width: 370px;
+  height: 1px;
+  margin: 0px -0.5px;
+`;
+const FlexRow1 = styled.div`
+  width: 339px;
+  gap: 69px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-self: stretch;
+  align-items: center;
+  padding: 0px 15px;
+`;
+const Text5 = styled.div`
+  color: #757575;
+  font-size: 12px;
+  font-weight: 700;
+  font-family: Inter;
+  white-space: nowrap;
+`;
+const NavyBlueFlexRow = styled.div`
+  gap: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 15px 20px;
+  border-radius: 15px;
+  background-color: #1678cb;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+const Text6 = styled.div`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 500;
+  font-family: Inter;
+  white-space: nowrap;
+`;
+const Arrow = styled.img`
+  width: 17px;
+  height: 14.7px;
+`;
---
+++ Form 2/app.js
@@ -0,0 +1,7 @@
+import { SignupFormRootRoot1 } from "./SignupFormRootRoot1";
+
+export default function App() {
+  document.body.style.backgroundColor = "#1e1e1e";
+  document.body.style.margin = "0";
+  return <SignupFormRootRoot1 />;
+}
